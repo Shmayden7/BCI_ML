4fcc4344172ced3d1e27f3ad61a9ea407e8af449
Ayden Cauchi <ayden@modimages.com>
2021-10-28 11:05:37 -0400

    Added example RF_alg, updated localRef file for newly named data

--- localDataRefrence.py
+++ localDataRefrence.py
@@ -1,30 +1,17 @@
-def getChannelValue(index):
-    channels = [
-        'Fp1','Fp2','F3','F4',
-        'C3','C4','P3','P4',
-        'O1','O2','A1','A2',
-        'F7','F8','T3','T4',
-        'T5','T6','Fz','Cz',
-        'Pz','Movement'
-    ]
-    return channels[index]
-
 def getRefAyden(index):
-    localPath = '/Users/Ayden/Documents/BCI/ML_Training/'
+    localPath = '/Users/Ayden/Documents/BCI/ML_Training/selectChannels/'
 
     localFiles = [
-        '5F-SubjectB-160309-5St-SGLHand-HFREQ.csv',
-        '5F-SubjectB-160311-5St-SGLHand-HFREQ.csv',
-        '5F-SubjectC-160429-5St-SGLHand-HFREQ.csv',
-        '5F-SubjectF-160210-5St-SGLHand-HFREQ.csv',
-        '5F-SubjectG-160413-5St-SGLHand-HFREQ.csv',
-        '5F-SubjectH-160804-5St-SGLHand-HFREQ.csv',
-        '5F-SubjectI-160719-5St-SGLHand-HFREQ.csv',
-        'CLA-SubjectJ-170504-3St-LRHand-Inter.csv',
-        'CLA-SubjectJ-170508-3St-LRHand-Inter.csv',
-        'CLA-SubjectJ-170510-3St-LRHand-Inter.csv',
-        '5F-SubjectI-160723-5St-SGLHand-HFREQ.csv',
-        '5F-SubjectE-160321-5St-SGLHand-HFREQ.csv'
+        'C-160224.csv',
+        'E-160304.csv',
+        'F-160202.csv',
+        'G-160412.csv',
+        'H-160720.csv',
+        'I-160628.csv',
+        'J-1611216.csv',
+        'K-1611086.csv',
+        'L-1612056.csv',
+        'M-1611176.csv',
     ]
     return (localPath + localFiles[index])
 
@@ -32,16 +19,34 @@ def getRefJosh(index):
     localPath = ''
 
     localFiles = [
-        '',
-        '',
-        '',
-        '',
-        '',
-        '',
-        '',
-        '',
-        '',
-        '',
+        'C-160224.csv',
+        'E-160304.csv',
+        'F-160202.csv',
+        'G-160412.csv',
+        'H-160720.csv',
+        'I-160628.csv',
+        'J-1611216.csv',
+        'K-1611086.csv',
+        'L-1612056.csv',
+        'M-1611176.csv',
+    ]
+
+    return (localPath + localFiles[index])
+
+def getRefAhmed(index):
+    localPath = ''
+
+    localFiles = [
+        'C-160224.csv',
+        'E-160304.csv',
+        'F-160202.csv',
+        'G-160412.csv',
+        'H-160720.csv',
+        'I-160628.csv',
+        'J-1611216.csv',
+        'K-1611086.csv',
+        'L-1612056.csv',
+        'M-1611176.csv',
     ]
 
     return (localPath + localFiles[index])
